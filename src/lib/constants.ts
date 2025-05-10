465307b9b25fa85168a5d7df7b035a051bd4b77d
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-05-10 21:46:17 +0200

    app/.*: add home page
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

---
+++ src/lib/constants.ts
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2025 Kiril Strezikozin
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * You may not use this file except in compliance with the MIT license terms.
+ */
+
+export const TrelloUrl = "https://trello.com";
+
+export const AppName = "Trello Notifications";
+export const AppDescription = "Notify a channel in Discord about activity in Trello";
+
+export const AppSourceUrl = "https://gitlab.com/KirilStrezikozin/discord-trello-activity-app";
+
+export const AppGuideUrl = "https://gitlab.com/KirilStrezikozin/discord-trello-activity-app#discord-trello-activity-app";
