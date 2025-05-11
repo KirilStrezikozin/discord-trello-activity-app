e76eb87d347803663563ebd64d358eb20f773051
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-05-11 23:13:16 +0200

    .*: add parse and find Action for Trello activity, use in route
    * Add findActionFor function to find an Activity type matching data.
    * Create parse.ts
    * Move UnsupportedActivityError from route.ts to parse.ts.
    * Use findActionFor in route.ts.
    
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -11,7 +11,13 @@ import { RequestMediaTypeError, verifiedRequestBody } from "@/src/lib/crypto";
 
 import { ActionError } from "@/src/lib/trello/action/types/error";
 
+import {
+  findActionFor,
+  UnsupportedActivityError
+} from "@/src/lib/trello/action/parse";
+
 import * as log from "@/src/lib/log";
+import { BoardModelSchema } from "@/src/lib/trello/action/schema";
 
 const TrelloWebhookSecret = process.env.TRELLO_WEBHOOK_SECRET ?? "";
 const DiscordWebhookUrl = process.env.DISCORD_WEBHOOK_URL ?? "";
@@ -21,18 +27,6 @@ const DiscordWebhookUrl = process.env.DISCORD_WEBHOOK_URL ?? "";
  * sent to discord in a special message format.
  */
 const ErrorsAsDiscordMessages = Boolean(process.env.ERRORS_AS_DISCORD_MESSAGES);
-
-/**
- * Thrown if the webhook request contains Trello activity
- * data that cannot be handled.
- */
-class UnsupportedActivityError extends Error {
-  constructor(public data: unknown) {
-    super("Unsupported activity type");
-    this.name = "UnsupportedActivityError";
-    this.data = data;
-  }
-}
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 export async function HEAD(_request: Request) {
@@ -50,27 +44,33 @@ export async function POST(request: Request) {
     /* Verify and parse the request body. */
     const body = await verifiedRequestBody(request, TrelloWebhookSecret);
 
-    // const action = ActionMoveCardFromListToList.from(body.action.data);
-    //
-    // if (action.success) {
-    //   const board = BoardModelSchema.safeParse(body.model).data;
-    //
-    //   discordClient.send(action.action.buildMessage({
-    //     member: body.action.memberCreator,
-    //     board: {
-    //       id: board?.id,
-    //       name: board?.name,
-    //     },
-    //   }));
-    // }
-
-    // TODO: add and use activity handlers (Actions).
-
-    throw new UnsupportedActivityError({
+    /* Try to find an Action type matching the Trello activity data.
+     * `UnsupportedActivityError` will be thrown on failure. */
+    const action = findActionFor({
       data: body.action.data,
       type: body.action.type,
       translationKey: body.action.display?.translationKey
     });
+
+    /* If the model the webhook is subscribed to is a Trello board, Discord
+     * message builder below gets more information about the activity. */
+    const board = BoardModelSchema.safeParse(body.model).data;
+
+    /* Use the matched Action to build a Discord message from
+     * Trello activity data it holds. Send the message. */
+    discordClient.send(action.buildMessage({
+      member: body.action.memberCreator,
+      board: {
+        id: board?.id,
+        name: board?.name,
+        prefs: {
+          backgroundColor: board?.prefs.backgroundColor,
+          backgroundDarkColor: board?.prefs.backgroundDarkColor,
+          backgroundBottomColor: board?.prefs.backgroundBottomColor,
+          backgroundTopColor: board?.prefs.backgroundTopColor,
+        },
+      },
+    }));
 
   } catch (error) {
     log.error(error);
---
+++ src/lib/trello/action/parse.ts
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2025 Kiril Strezikozin
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * You may not use this file except in compliance with the MIT license terms.
+ */
+
+import { Action } from "./types/base";
+
+/**
+ * Thrown if the webhook request contains Trello activity
+ * data that cannot be handled.
+ */
+export class UnsupportedActivityError extends Error {
+  constructor(public data: unknown) {
+    super("Unsupported activity type");
+    this.name = "UnsupportedActivityError";
+    this.data = data;
+  }
+}
+
+/**
+ * List of all Action types in the library.
+ */
+export const ActionTypes = [
+];
+
+/**
+ * Find the Action type in the library the schema of which is satisfied by the
+ * given data. Otherwise, an error is thrown.
+ *
+ * @param data Trello activity data to find a matching Action type for.
+ * @returns Matched Action type from the ones defined in the library on success.
+ *
+ * @throws {UnsupportedActivityError} When failed to find a matching Action.
+ */
+export function findActionFor(data: unknown): Action {
+  for (const at of ActionTypes) {
+    const ActionInferred = at as (typeof Action);
+
+    /* If the given data satisfies Action's schema, we found a match! */
+    const res = ActionInferred.from(data);
+    if (res.success) {
+      return res.action;
+    }
+  }
+
+  throw new UnsupportedActivityError(data);
+}
