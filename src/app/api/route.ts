9b18c45b48a925ffba914120143d1daae0d69c3b
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-05-11 18:33:31 +0200

    api/route.ts: add use ActionError, error reporting in Discord
    * Add use ActionError to respond to Trello activity types (all are
    handled as unsupported in this commit).
    * Add conditionally report API server errors as Discord messages.

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,13 +6,33 @@
  * You may not use this file except in compliance with the MIT license terms.
  */
 
+import { WebhookClient } from "discord.js";
 import { RequestMediaTypeError, verifiedRequestBody } from "@/src/lib/crypto";
-import { sendMessage } from "@/src/lib/discord";
+
+import { ActionError } from "@/src/lib/trello/action/types/error";
 
 import * as log from "@/src/lib/log";
 
-const TrelloWebhookSecret = process.env.TRELLO_WEBHOOK_SECRET || "";
-const DiscordWebhookUrl = process.env.DISCORD_WEBHOOK_URL || "";
+const TrelloWebhookSecret = process.env.TRELLO_WEBHOOK_SECRET ?? "";
+const DiscordWebhookUrl = process.env.DISCORD_WEBHOOK_URL ?? "";
+
+/**
+ * True when catchable server errors should be
+ * sent to discord in a special message format.
+ */
+const ErrorsAsDiscordMessages = Boolean(process.env.ERRORS_AS_DISCORD_MESSAGES);
+
+/**
+ * Thrown if the webhook request contains Trello activity
+ * data that cannot be handled.
+ */
+class UnsupportedActivityError extends Error {
+  constructor(public data: unknown) {
+    super("Unsupported activity type");
+    this.name = "UnsupportedActivityError";
+    this.data = data;
+  }
+}
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 export async function HEAD(_request: Request) {
@@ -24,12 +44,33 @@ export async function HEAD(_request: Request) {
 
 export async function POST(request: Request) {
   /* Read the request body and process webhook payload. */
+  const discordClient = new WebhookClient({ url: DiscordWebhookUrl });
 
   try {
     /* Verify and parse the request body. */
     const body = await verifiedRequestBody(request, TrelloWebhookSecret);
 
-    sendMessage(DiscordWebhookUrl, JSON.stringify(body.action.data, null, 2));
+    // const action = ActionMoveCardFromListToList.from(body.action.data);
+    //
+    // if (action.success) {
+    //   const board = BoardModelSchema.safeParse(body.model).data;
+    //
+    //   discordClient.send(action.action.buildMessage({
+    //     member: body.action.memberCreator,
+    //     board: {
+    //       id: board?.id,
+    //       name: board?.name,
+    //     },
+    //   }));
+    // }
+
+    // TODO: add and use activity handlers (Actions).
+
+    throw new UnsupportedActivityError({
+      data: body.action.data,
+      type: body.action.type,
+      translationKey: body.action.display?.translationKey
+    });
 
   } catch (error) {
     log.error(error);
@@ -39,7 +80,7 @@ export async function POST(request: Request) {
 
     if (error instanceof RequestMediaTypeError) {
       message = error.message;
-      status = 415;
+      status = 415; /* Unsupported content-type. */
     }
 
     else if (error instanceof Error) {
@@ -51,6 +92,22 @@ export async function POST(request: Request) {
       message = "unknown error";
       status = 400;
     }
+
+    /* Report and send API server error as a Discord messages.
+     * Response status code is not consumed for Trello to sense the
+     * error and ultimately retry a request. */
+    if (ErrorsAsDiscordMessages && error instanceof Error) {
+      let actionData = undefined;
+      if (error instanceof UnsupportedActivityError) {
+        actionData = error.data;
+      }
+
+      const action = ActionError.from({ error: error, action: actionData });
+      if (action.success) {
+        discordClient.send(action.action.buildMessage({}));
+      }
+    }
+
 
     return new Response(`Error: ${message}`, { status: status });
   }
