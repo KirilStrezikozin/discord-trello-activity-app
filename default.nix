0665f7173d5c6b18cca5b07830eef10a6ded874c
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-07-04 14:18:10 +0200

    default.nix: update set allowUnfree in config
    Avoid the need to configure allowUnfree for ngrok installation to
    proceed through environment variable or system config.
    
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- default.nix
+++ default.nix
@@ -1,7 +1,13 @@
-{pkgs ? import <nixpkgs> {}}:
+let
+  # Configure Nix to allow unfree packages.
+  config = {
+    allowUnfree = true;
+  };
+  pkgs = import <nixpkgs> { inherit config; };
+in
 pkgs.mkShell {
   packages = with pkgs; [
     corepack
-    ngrok
+    ngrok # Unfree package.
   ];
 }
