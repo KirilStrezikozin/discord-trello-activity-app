d9a914bc40a5a71649312c175b7a179ef1d68fd2
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-07-08 11:08:58 +0200

    test/.../action/types/.../format.sh: update raw payload filtering
    * Add hide URL values
    * Add hide Email values
    * Add hide more ID values
    * Add filter plugin and member if these keys exist
    
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- test/lib/trello/action/types/_payloads/format.sh
+++ test/lib/trello/action/types/_payloads/format.sh
@@ -14,22 +14,47 @@ if ! command -v jq >/dev/null 2>&1; then
 fi
 
 filtered=$(
-jq '
-{
+    jq '
+({
     data: .action.data,
     type: .action.type,
     translationKey: .action.display.translationKey
 } 
++ (if .action | has("member") then {member: .action.member} else {} end)
++ (if .action | has("plugin") then {plugin: .action.plugin} else {} end))
 |
 walk(
     if type == "object" then
+        (
+            reduce keys[] as $k (
+                .;
+                if ($k | ascii_downcase | contains("url")) then
+                    .[$k] = "https://example.com"
+                elif ($k | ascii_downcase | contains("email")) then
+                    .[$k] = "myemail@myemail.com"
+                else
+                    .
+                end
+            )
+        ) |
         (if has("name") then .name = "test" else . end) |
         (if has("desc") then .desc = "test" else . end) |
+        (if has("text") then .text = "test" else . end) |
         (if has("id") then .id = "1234567890" else . end) |
         (if has("idShort") then .idShort = 123 else . end) |
+        (if has("locale") then .locale = "en-us" else . end) |
+        (if has("overview") then .overview = "test" else . end) |
         (if has("shortLink") then .shortLink = "abc" else . end) |
+        (if has("idCard") then .idCard = "1234567890"  else . end) |
         (if has("idList") then .idList = "1234567890"  else . end) |
-        (if has("idMember") then .idMember = "1234567890" else . end)
+        (if has("description") then .description = "test" else . end) |
+        (if has("idMember") then .idMember = "1234567890" else . end) |
+        (if has("idLabels") then .idLabels |= map("1234567890") else . end) |
+        (if has("idAttachment") then .idAttachment = "1234567890" else . end) |
+        (if has("idMemberAdded") then .idMemberAdded = "1234567890" else . end) |
+        (if has("avatarHash") then .avatarHash = "abc1234567890abc" else . end) |
+        (if has("idAttachmentCover") then .idAttachmentCover = "1234567890" else . end) |
+        (if has("idOrganizationOwner") then .idOrganizationOwner = "1234567890" else . end)
     else
         .
     end
