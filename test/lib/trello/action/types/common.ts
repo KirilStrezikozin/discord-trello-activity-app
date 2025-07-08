2b13791130812d0919ed557417409dc0c639d09b
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-07-08 11:14:51 +0200

    test/.../action/types/common.ts: refactor JSON payload collection
    * Refactor JSON payload collection to support .0 .1 payload indices.
    Payloads are collected in a Map instead of an array of tuples.
    * Refactor getPayloadsExceptFor from array generator into an iterator.
    
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- test/lib/trello/action/types/common.ts
+++ test/lib/trello/action/types/common.ts
@@ -7,45 +7,59 @@
  */
 
 import * as fs from "fs";
+import * as path from "path";
 
 const payloadDirectory = "./test/lib/trello/action/types/_payloads/";
 
 /**
- * All pre-made JSON payloads for all Trello action types described in the
- * project. Each element is a tuple, where the first element is an action type
- * name, the second is the JSON payload ready for parsing.
+ * Map of pre-made JSON payload names for all Trello action types described in
+ * the project. Keys are action type names, values are payload file names.
  */
-export const payloads: [string, object][] = fs.readdirSync(payloadDirectory)
-  .filter((filePath) => (/^[A-Z]$/.test(filePath[0])))
-  .map((filePath) => {
-    const name = filePath.replace(/\.[^\.]*$/, "");
-    const fileData = fs.readFileSync(payloadDirectory + filePath, "utf-8");
+export const payloadNames: Map<string, string[]> = new Map();
+fs.readdirSync(payloadDirectory)
+  .forEach((value) => {
+    /* Filter filenames starting with a capital letter. */
+    if (!/^[A-Z]$/.test(value[0])) return;
+    const fileName = path.parse(value).name;
+    const typeName = path.parse(fileName).name;
+    payloadNames.set(typeName, payloadNames.get(typeName) ?? []);
+    payloadNames.get(typeName)!.push(fileName);
+  });
+
+/**
+ * Map of pre-made JSON payloads for all Trello action types described in
+ * the project. Keys are action type names, values are payload objects.
+ */
+export const payloads: Map<string, object[]> = new Map();
+payloadNames.forEach((value, key) => {
+  payloads.set(key, value.map((payloadName) => {
+    const fileData = fs.readFileSync(
+      payloadDirectory + payloadName + ".json", "utf-8"
+    );
 
     try {
-      const payload = JSON.parse(fileData);
-      return [name, payload];
+      return JSON.parse(fileData);
     } catch (error) {
       let message = "unknown";
       if (error instanceof Error) {
         message = error.message;
       }
 
-      throw new Error(
-        `Error parsing payload content for ${name}, error: ${message}`
-      );
+      throw new Error(`Error parsing ${payloadName}.json, error: ${message}`);
     }
-  });
+  }));
+});
 
 /**
- * Get a list of pre-made JSON payloads for all Trello action types described in
- * the project except the name of the given one.
- *
- * Each element in the returned list is a tuple, where the first element is an
- * action type name, the second is the JSON payload ready for parsing.
+ * Returns an iterable of pre-made JSON payloads for all Trello action types
+ * described in the project except the ones grouped by the given type name.
  *
  * @param skipName - Action type name to skip.
- * @returns - List of pre-made JSON payloads.
+ * @returns Iterable of pre-made JSON payloads.
  */
-export function getPayloadsExceptFor(skipName: string): [string, object][] {
-  return payloads.filter(([name]) => name !== skipName);
+export function* getPayloadsExceptFor(skipName: string): Generator<object, void, unknown> {
+  for (const [key, values] of payloads.entries()) {
+    if (key === skipName) continue;
+    for (const x of values) yield x;
+  }
 }
