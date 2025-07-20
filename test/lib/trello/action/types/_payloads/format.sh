99e04e16e840fd2f6536c52ebc75a4a32fba4fe5
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-07-20 17:05:47 +0200

    test/.../_payloads/format.sh: update skip modifying null values
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- test/lib/trello/action/types/_payloads/format.sh
+++ test/lib/trello/action/types/_payloads/format.sh
@@ -38,24 +38,24 @@ walk(
                 end
             )
         ) |
-        (if has("name") then .name = "test" else . end) |
-        (if has("desc") then .desc = "test" else . end) |
-        (if has("text") then .text = "test" else . end) |
-        (if has("id") then .id = "1234567890" else . end) |
-        (if has("idShort") then .idShort = 123 else . end) |
-        (if has("locale") then .locale = "en-us" else . end) |
-        (if has("overview") then .overview = "test" else . end) |
-        (if has("shortLink") then .shortLink = "abc" else . end) |
-        (if has("idCard") then .idCard = "1234567890"  else . end) |
-        (if has("idList") then .idList = "1234567890"  else . end) |
-        (if has("description") then .description = "test" else . end) |
-        (if has("idMember") then .idMember = "1234567890" else . end) |
-        (if has("idLabels") then .idLabels |= map("1234567890") else . end) |
-        (if has("idAttachment") then .idAttachment = "1234567890" else . end) |
-        (if has("idMemberAdded") then .idMemberAdded = "1234567890" else . end) |
-        (if has("avatarHash") then .avatarHash = "abc1234567890abc" else . end) |
-        (if has("idAttachmentCover") then .idAttachmentCover = "1234567890" else . end) |
-        (if has("idOrganizationOwner") then .idOrganizationOwner = "1234567890" else . end)
+        (if has("name") and .name != null then .name = "test" else . end) |
+        (if has("desc") and .desc != null then .desc = "test" else . end) |
+        (if has("text") and .text != null then .text = "test" else . end) |
+        (if has("id") and .id != null then .id = "1234567890" else . end) |
+        (if has("idShort") and .idShort != null then .idShort = 123 else . end) |
+        (if has("locale") and .locale != null then .locale = "en-us" else . end) |
+        (if has("overview") and .overview != null then .overview = "test" else . end) |
+        (if has("shortLink") and .shortLink != null then .shortLink = "abc" else . end) |
+        (if has("idCard") and .idCard != null then .idCard = "1234567890"  else . end) |
+        (if has("idList") and .idList != null then .idList = "1234567890"  else . end) |
+        (if has("description") and .description != null then .description = "test" else . end) |
+        (if has("idMember") and .idMember != null then .idMember = "1234567890" else . end) |
+        (if has("idLabels") and .idLabels != null then .idLabels |= map("1234567890") else . end) |
+        (if has("idAttachment") and .idAttachment != null then .idAttachment = "1234567890" else . end) |
+        (if has("idMemberAdded") and .idMemberAdded != null then .idMemberAdded = "1234567890" else . end) |
+        (if has("avatarHash") and .avatarHash != null then .avatarHash = "abc1234567890abc" else . end) |
+        (if has("idAttachmentCover") and .idAttachmentCover != null then .idAttachmentCover = "1234567890" else . end) |
+        (if has("idOrganizationOwner") and .idOrganizationOwner != null then .idOrganizationOwner = "1234567890" else . end)
     else
         .
     end
