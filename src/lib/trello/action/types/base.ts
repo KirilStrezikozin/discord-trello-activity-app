c190aa46224f6b64e166ef0d6c537f5fab1d6b96
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2025-07-21 14:49:05 +0200

    .../action/types/base.ts: add overload getMemberIcon with direct url as parameter
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- src/lib/trello/action/types/base.ts
+++ src/lib/trello/action/types/base.ts
@@ -43,9 +43,42 @@ export type MessageOptions = {
 
 /**
  * Returns an icon URL for a member who triggered the action.
+ *
+ * @param opts Options provide information about the action's context.
+ * @param iconSizePixels Icon size in pixels. If not given, use the size set in
+ * options. If not set in options, default value is defaultIconSizePixels.
+ *
+ * @returns Icon URL if avatarUrl is set in the given options.
  */
-export function getMemberIcon(opts: MessageOptions): string | undefined {
-  const iconSizePixels = opts.iconSizePixels ?? defaultIconSizePixels;
+export function getMemberIcon(opts: MessageOptions, iconSizePixels?: number): string | undefined;
+
+/**
+ * Returns a ready-to-use member icon URL.
+ *
+ * @param iconUrl Raw URL of the icon.
+ * @param iconSizePixels Icon size in pixels. Default is set by defaultIconSizePixels.
+ *
+ * @returns Icon URL.
+ */
+export function getMemberIcon(iconUrl: string, iconSizePixels?: number): string;
+
+export function getMemberIcon(
+  opts: MessageOptions | string,
+  iconSizePixels: number | undefined
+): string | undefined {
+  if (!iconSizePixels) {
+    iconSizePixels = defaultIconSizePixels;
+  }
+
+  if (typeof opts === "string") {
+    const iconUrl = opts;
+    return `${iconUrl}/${iconSizePixels}.png`;
+  }
+
+  if (opts.iconSizePixels) {
+    iconSizePixels = opts.iconSizePixels;
+  }
+
   return (opts.member)
     ? `${opts.member?.avatarUrl}/${iconSizePixels}.png`
     : undefined;
